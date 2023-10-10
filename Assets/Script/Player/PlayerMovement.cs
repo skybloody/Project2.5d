d49f232d338b9aeecbcb40746db5d8c75ec0331d
peesasidech <129536390+Skyusehot007@users.noreply.github.com>
2023-10-10 20:57:08 +0700

    project

---
+++ Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody theRB;
+    public float moveSpeed, jumpForce;
+    private Vector2 moveInput;
+    public LayerMask whatIsGround;
+    public Transform groundPoint;
+    private bool isGrounded;
+
+
+     void Start()
+    {
+  
+    }
+
+     void Update()
+    {
+        moveInput.y = Input.GetAxis("Vertical");
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.Normalize();
+
+        theRB.velocity = new Vector3(moveInput.x * moveSpeed, theRB.velocity.y, moveInput.y * moveSpeed);
+       
+        RaycastHit hit;
+        if (Physics.Raycast(groundPoint.position, Vector3.down, out hit, .3f, whatIsGround))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        { 
+         theRB.velocity += new Vector3(0f, jumpForce, 0f); 
+        }
+    }
+}
